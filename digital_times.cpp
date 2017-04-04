641867f5abc925fb493dc7596585df100edb05e9
cunteng008 <2595400537@qq.com>
2017-04-05 07:46:01 +0800

    first commit

---
+++ digital_times.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+
+typedef int DIGITAL;
+typedef int APPEAR_TIMES;  //出现次数
+using namespace std;
+int main(){
+    int x;
+    map<DIGITAL,APPEAR_TIMES> all;
+    while(cin>>x &&cin.get()==','){
+       all[x]++;
+    }
+    map<DIGITAL,APPEAR_TIMES>::iterator iter;
+    for(iter=all.begin();iter!=all.end();iter++){
+        cout<<iter->first<<"("<<iter->second<<")"<<endl;
+    }
+    return 0;
+}
---
+++ insert_perfix.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int insert(vector<string>& words,string word);  //插入
+vector<string> findPerfixWords(const vector<string>& words,string perfix);
+
+int main(){
+    int n,m;
+    vector<string> words;
+    string word;
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        cin>>word;
+        words.push_back(word);
+    }
+
+    string order;
+    vector<string> ws;
+    for(int i=0;i<m;i++){
+        cin>>order;
+        if(order=="insert"){
+            cin>>word;
+            cout<<insert(words,word)<<endl;
+        }
+       
+        if(order == "perfix"){
+           cin>>word;
+           ws = findPerfixWords(words,word);
+           for(auto w:ws){
+               cout<<w<<endl;
+           }
+        }
+
+    }
+    
+    return 0;
+}
+
+int insert(vector<string>& words,string word){
+    vector<string>::iterator it;
+    it = find(words.begin(),words.end(),word);
+    if(it!=words.end()){
+       
+        return 1;
+    }
+    words.push_back(word);
+    return 0;
+}
+
+vector<string> findPerfixWords(const vector<string>& words,string perfix){
+    vector<string> ws;
+    unsigned int len = perfix.size();
+    for(string w:words){
+        if(w.size()<len){
+            continue;
+        }
+        string str = w.substr(0,len);
+        if(str==perfix){
+            ws.push_back(w);
+        }
+    }
+    return ws;
+}
